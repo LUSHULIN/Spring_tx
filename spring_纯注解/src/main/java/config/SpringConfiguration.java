7cdb85851e2d1bea85f4f0c2b07feaa45517fb56
lushulin <java_lin@163.com>
2019-11-24 23:49:29 +0800

    submit

---
+++ spring_纯注解/src/main/java/config/SpringConfiguration.java
@@ -0,0 +1,17 @@
+package config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+//spring配置类
+@Configuration
+@ComponentScan("com.itheima")
+@Import({JdbcConfig.class,TransactionConfig.class})
+@PropertySource("jdbcConfig.properties")
+@EnableTransactionManagement//开启事务
+public class SpringConfiguration {
+
+}
